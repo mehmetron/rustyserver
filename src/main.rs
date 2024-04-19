0b7de04870233720c99b89d3aa724fc40617d83f
mehmetron <mtcureoglu@gmail.com>
2024-04-18 22:13:22 -0500

    created initial webserver

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{http::StatusCode, routing::get, Router};
+
+async fn index() -> String {
+    String::from("homepage")
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(index));
+
+    axum::Server::bind(&"0.0.0.0:4000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
